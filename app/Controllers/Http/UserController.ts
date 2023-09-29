261f350c601d83073a6f84d6943f20a729ad6b8d
Roman Antipov <r.antipov@univertus.com>
2023-09-29 15:20:09 +0300

    feat: unauthorized error

--- app/Controllers/Http/UserController.ts
+++ app/Controllers/Http/UserController.ts
@@ -1,7 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import UnAuthorized from 'App/Exceptions/UnAuthorizedException'
+
 export default class UserController {
   public async getUser({ auth, response }: HttpContextContract) {
-    return response.send(auth.user)
+    if (auth.user) {
+      return response.send(auth.user)
+    }
+
+    throw new UnAuthorized('You are not authorized', 401, 'E_UNAUTHORIZED')
   }
 }
